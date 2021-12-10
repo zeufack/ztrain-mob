81e768c21ef231e607cfbf857e0f6cbc88148ca9
zeufack <zeufackp@gmail.com>
2021-12-10 12:59:11 +0100

    add image on empty cart

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/models/Cart.dart';
 import 'package:shop_app/models/product_dao.dart';
+import 'package:shop_app/screens/cart/components/empty_cart.dart';
 
 import '../../../size_config.dart';
 import 'cart_card.dart';
@@ -30,38 +31,40 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     ProductDAO productDAO = Provider.of<ProductDAO>(context, listen: true);
-    return Padding(
-      padding:
-          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: ListView.builder(
-        itemCount: carts.length,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Dismissible(
-            key: UniqueKey(),
-            direction: DismissDirection.endToStart,
-            onDismissed: (direction) {
-              productDAO.deletedFromCard(carts[index].id);
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text('retirer du panier')));
-            },
-            background: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFE6E6),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                children: [
-                  Spacer(),
-                  SvgPicture.asset("assets/icons/Trash.svg"),
-                ],
+    return carts.isEmpty
+        ? EmptyCart()
+        : Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20)),
+            child: ListView.builder(
+              itemCount: carts.length,
+              itemBuilder: (context, index) => Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Dismissible(
+                  key: UniqueKey(),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    productDAO.deletedFromCard(carts[index].id);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('retirer du panier')));
+                  },
+                  background: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFE6E6),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        SvgPicture.asset("assets/icons/Trash.svg"),
+                      ],
+                    ),
+                  ),
+                  child: CartCard(cart: carts[index]),
+                ),
               ),
             ),
-            child: CartCard(cart: carts[index]),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
